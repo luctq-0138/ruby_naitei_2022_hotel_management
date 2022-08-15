311df83a3b85851dbc64b888cf36f759793249bb
luctq <tranquocluc610@gmail.com>
2022-08-15 10:11:42 +0700

    test import javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,15 @@ import Rails from '@rails/ujs';
 import Turbolinks from 'turbolinks';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
+import 'owl.carousel';
+require('jquery');
 require('bootstrap');
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+function importAll(r) {
+  r.keys().forEach(r);
+}
+importAll(require.context('./js', true, /\.js$/));
+importAll(require.context('./js', true, /\.min\.js$/));
