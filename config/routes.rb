62cf7bc45d986c78ffcac6361b72dda18d798a10
VuTan115 <tanboy042001@gmail.com>
2022-08-15 14:26:20 +0700

    task_52383_homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
+    get "/rooms", to: "static_pages#rooms"
+    get "/about_us", to: "static_pages#about_us"
+    get "/blog", to: "static_pages#blog"
+    get "/contact", to: "static_pages#contact"
     get "/signup", to: "users#new"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
@@ -8,6 +12,9 @@ Rails.application.routes.draw do
     resources :users
     namespace :admin do
       root to: "static_pages#index"
+      get "/rooms", to: "static_pages#rooms"
+      get "/bookings", to: "static_pages#bookings"
+      get "/users", to: "static_pages#users"
       resources :static_pages
     end
   end
---
+++ spec/requests/admin/booking_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Admin::Bookings", type: :request do
+
+end
