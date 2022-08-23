cd12a00c9e7c06e82c4d3d111f6f6c259071339e
luctq <tranquocluc610@gmail.com>
2022-08-24 00:55:13 +0700

    Task: Toast message

--- app/javascript/packs/application_admin.js
+++ app/javascript/packs/application_admin.js
@@ -5,6 +5,7 @@ import 'channels';
 import 'owl.carousel';
 require('jquery');
 require('bootstrap');
+import '../stylesheets/application';
 function importAll(r) {
   r.keys().forEach(r);
 }
@@ -13,3 +14,21 @@ importAll(require.context('./admin/js', true, /\.min\.js$/));
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+global.toastr = require('toastr');
+toastr.options = {
+  closeButton: false,
+  debug: false,
+  newestOnTop: false,
+  progressBar: true,
+  positionClass: 'toast-top-right',
+  preventDuplicates: false,
+  onclick: null,
+  showDuration: '300',
+  hideDuration: '1000',
+  timeOut: '5000',
+  extendedTimeOut: '1000',
+  showEasing: 'swing',
+  hideEasing: 'linear',
+  showMethod: 'fadeIn',
+  hideMethod: 'fadeOut',
+};
